8ceb964fde64eca35ba625345c13456a3fb2f1b5
ktechhydle <151480646+ktechhydle@users.noreply.github.com>
2024-04-10 08:07:07 -0600

    Update custom_classes.py

--- custom_classes.py
+++ custom_classes.py
@@ -80,7 +80,7 @@ class CustomGraphicsView(QGraphicsView):
         self.canvas = canvas
         self.temp_path_item = None
         self.pen = None
-		self.lp = None
+        self.lp = None
 
     def update_pen(self, pen):
         self.pen = pen
