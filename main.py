e181ffe5473044eb228c4d4d337a3bd47c8d5e8d
ktechhydle <151480646+ktechhydle@users.noreply.github.com>
2024-04-10 19:53:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,6 @@ from PyQt5.QtCore import *
 from PyQt5.QtSvg import *
 from custom_classes import *
 
-class item_stack:
-    def __init__(self, initial_value=""):
-        self._value = initial_value
-
-    def set(self, value):
-        self._value = value
-
-    def get(self):
-        return self._value
-
 class MPRUN(QMainWindow):
     def __init__(self):
         super().__init__()
