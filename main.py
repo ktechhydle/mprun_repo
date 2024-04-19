e5f2f05483dd68e749eca58aea9860a814fbc1e3
K-TECH Industries (Keller Hydle) <151480646+ktechhydle@users.noreply.github.com>
2024-04-18 18:46:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1269,8 +1269,7 @@ class CourseElementsWin(QWidget):
     def create_image_attributes(self, item):
         item.setFlag(QGraphicsItem.ItemIsMovable)
         item.setFlag(QGraphicsItem.ItemIsSelectable)
-        item.setPos(450, 300)
-        item.setZValue(1)
+        item.setZValue(0)
 
     def closeEvent(self, event):
         # Display a confirmation dialog
