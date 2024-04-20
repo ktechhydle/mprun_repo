0cfc1d063a82d8ab767cf49a776d95d1687d51a0
kelle <kellerhydle@gmail.com>
2024-04-19 20:15:52 -0600

    added methods for placing widgets horizontally on a qtoolbar

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from PyQt5.QtCore import *
 from PyQt5.QtSvg import *
 from graphics_framework import *
 from custom_classes import *
+from custom_widgets import *
+from course_elements import *
 from pixels2svg import *
 
 class MPRUN(QMainWindow):
@@ -128,6 +130,12 @@ class MPRUN(QMainWindow):
         self.entry3.textChanged.connect(self.use_scale_y)
         self.entry3.setPlaceholderText("Vertical scale factor")
 
+        self.scale_from_center_check_btn = QCheckBox(self)
+        scale_from_center_label = QLabel('Scale From Center')
+        widget2 = ToolbarHorizontalLayout()
+        widget2.layout.addWidget(self.scale_from_center_check_btn)
+        widget2.layout.addWidget(scale_from_center_label)
+
         # Stroke size spinbox
         self.stroke_size_spin = QSpinBox()
         self.stroke_size_spin.setValue(3)
@@ -139,13 +147,11 @@ class MPRUN(QMainWindow):
         self.color_tolerance_spin.setValue(256)
 
         # Vector convert background widgets
-        widget1 = QWidget(self)
-        layout1 = QHBoxLayout()
-        widget1.setLayout(layout1)
+        widget1 = ToolbarHorizontalLayout()
         background_remove_label = QLabel('Remove Background')
         self.background_remove_check_btn = QCheckBox(self)
-        layout1.addWidget(self.background_remove_check_btn)
-        layout1.addWidget(background_remove_label)
+        widget1.layout.addWidget(self.background_remove_check_btn)
+        widget1.layout.addWidget(background_remove_label)
 
         # Layer Combobox
         self.layer_options = {'Layer 0 (Default)': 0,'Layer 1 (Course Elements)': 1, 'Layer 2 (Lines/Paths)': 2, 'Layer 3 (Text/Labels)': 3}
@@ -171,17 +177,15 @@ class MPRUN(QMainWindow):
         Key-2''')
         lower_layer_btn.setShortcut(QKeySequence('2'))
         lower_layer_btn.clicked.connect(self.use_lower_layer)
-        bring_to_front_btn = QPushButton(QIcon('logos and icons/Tool Icons/raise_layer_icon.png'), '', self)
+        bring_to_front_btn = QPushButton(QIcon('logos and icons/Tool Icons/set_always_on_top_icon.png'), '', self)
         bring_to_front_btn.setToolTip('''Shortcut:
         Key-B''')
         bring_to_front_btn.setShortcut(QKeySequence('B'))
         bring_to_front_btn.clicked.connect(self.use_bring_to_front)
-        horizontal_widget_for_layer_buttons = QWidget()
-        horizontal_layout_for_layer_buttons = QHBoxLayout()
-        horizontal_widget_for_layer_buttons.setLayout(horizontal_layout_for_layer_buttons)
-        horizontal_layout_for_layer_buttons.addWidget(raise_layer_btn)
-        horizontal_layout_for_layer_buttons.addWidget(lower_layer_btn)
-        horizontal_layout_for_layer_buttons.addWidget(bring_to_front_btn)
+        horizontal_widget_for_layer_buttons = ToolbarHorizontalLayout()
+        horizontal_widget_for_layer_buttons.layout.addWidget(raise_layer_btn)
+        horizontal_widget_for_layer_buttons.layout.addWidget(lower_layer_btn)
+        horizontal_widget_for_layer_buttons.layout.addWidget(bring_to_front_btn)
 
         # Fill Color Button
         self.fill_color_btn = QPushButton('', self)
@@ -383,6 +387,7 @@ class MPRUN(QMainWindow):
         self.action_toolbar.addWidget(self.entry1)
         self.action_toolbar.addWidget(self.entry2)
         self.action_toolbar.addWidget(self.entry3)
+        self.action_toolbar.addWidget(widget2)
         self.action_toolbar.addWidget(opacity_label)
         self.action_toolbar.addWidget(self.opacity_slider)
         self.action_toolbar.addSeparator()
@@ -745,21 +750,40 @@ Date:   """)
 
     def use_scale_all(self, value):
         try:
-            value = float(value)
-            items = self.canvas.selectedItems()
-            for item in items:
-                # Calculate value
-                scale_factor = value / 100.0
-                scale = 0.1 + (scale_factor * 9.9)
+            if self.scale_from_center_check_btn.isChecked():
+                value = float(value)
+                items = self.canvas.selectedItems()
+                for item in items:
+                    # Calculate value
+                    scale_factor = value / 100.0
+                    scale = 0.1 + (scale_factor * 9.9)
 
-                # Calculate the center point of the item
-                center = item.boundingRect().center()
+                    # Calculate the center point of the item
+                    center = item.boundingRect().center()
 
-                # Set the transformation origin to the center point
-                item.setTransformOriginPoint(center)
+                    # Set the transformation origin to the center point
+                    item.setTransformOriginPoint(center)
 
-                # Scale item
-                item.setScale(scale)
+                    # Scale item
+                    item.setScale(scale)
+
+            else:
+                value = float(value)
+                items = self.canvas.selectedItems()
+                for item in items:
+                    # Calculate value
+                    scale_factor = value / 100.0
+                    scale = 0.1 + (scale_factor * 9.9)
+
+                    # Calculate the point of the item
+                    point_x, point_y = item.boundingRect().x(), item.boundingRect().y()
+
+                    # Set the transformation origin to the point
+                    item.setTransformOriginPoint(point_x, point_y)
+
+                    # Scale item
+                    item.setScale(scale)
+
         except ValueError:
             pass
 
@@ -1154,156 +1178,6 @@ Date:   """)
 
         # Refit screen
         self.use_refit_screen()
-
-class CourseElementsWin(QWidget):
-    def __init__(self, canvas):
-        super().__init__()
-
-        self.canvas = canvas
-
-        self.setWindowTitle('Course Elements Picker')
-        self.setWindowIcon(QIcon('logos and icons/MPRUN_logo_rounded_corners_version.png'))
-        self.setGeometry(50, 100, 300, 250)
-        self.setWindowFlags(self.windowFlags() | Qt.WindowStaysOnTopHint)
-
-        self.create_ui()
-
-    def create_ui(self):
-        # Create a layout
-        self.layout = QVBoxLayout()
-
-        # Create a scroll area
-        scroll_area = QScrollArea()
-        scroll_area.setWidgetResizable(True)
-        scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
-
-        # Create a widget to hold the layout
-        widget = QWidget()
-        widget.setLayout(self.layout)
-
-        # Set the widget as the scroll area's widget
-        scroll_area.setWidget(widget)
-
-        # Set the layout of the main window
-        main_layout = QVBoxLayout()
-        main_layout.addWidget(scroll_area)
-
-        self.setLayout(main_layout)
-
-        self.create_buttons()
-
-    def create_buttons(self):
-        # Course Element Spawn Buttons
-        btn1 = QPushButton()
-        btn1.setText('Rail 1 (Long Tube)')
-        btn1.clicked.connect(lambda: self.create_img('Course Element/rail 1 (long tube).svg'))
-
-        btn2 = QPushButton()
-        btn2.setText('Rail 1 (Short Tube)')
-        btn2.clicked.connect(lambda: self.create_img('Course Element/rail 1 (short tube).svg'))
-
-        btn3 = QPushButton()
-        btn3.setText('Jump 1 (Small)')
-        btn3.clicked.connect(lambda: self.create_img('Course Element/jump 1 (small).svg'))
-
-        btn4 = QPushButton()
-        btn4.setText('Jump 1 (Medium)')
-        btn4.clicked.connect(lambda: self.create_img('Course Element/jump 1 (medium).svg'))
-
-        btn5 = QPushButton()
-        btn5.setText('Jump 1 (Large)')
-        btn5.clicked.connect(lambda: self.create_img('Course Element/jump 1 (large).svg'))
-
-        btn6 = QPushButton()
-        btn6.setText("Halfpipe 1 (22')")
-        btn6.clicked.connect(lambda: self.create_img("Course Element/halfpipe 1 (22').svg"))
-
-        btn7 = QPushButton()
-        btn7.setText("Rail 2 (Skinny)")
-        btn7.clicked.connect(lambda: self.create_img("Course Element/rail 2 (skinny).svg"))
-
-        # Other Buttons
-        import_btn = QPushButton()
-        import_btn.setText('Import Course Element...')
-        import_btn.clicked.connect(self.import_course_element)
-
-        # Labels
-        rail_label = QLabel('Rails:')
-        jump_label = QLabel('Jumps:')
-        halfpipe_label = QLabel('Half/Quarter Pipes:')
-        import_label = QLabel('Imported Course Elements:')
-
-        # Add buttons to layout
-        self.layout.addWidget(rail_label)
-        self.layout.addWidget(btn1)
-        self.layout.addWidget(btn2)
-        self.layout.addWidget(btn7)
-        self.layout.addWidget(jump_label)
-        self.layout.addWidget(btn3)
-        self.layout.addWidget(btn4)
-        self.layout.addWidget(btn5)
-        self.layout.addWidget(halfpipe_label)
-        self.layout.addWidget(btn6)
-        self.layout.addWidget(import_label)
-        self.layout.addWidget(import_btn)
-
-    def create_img(self, svg_file):
-        svg_item = CustomSvgItem(svg_file)
-        svg_item.store_filename(svg_file)
-        self.canvas.addItem(svg_item)
-        svg_item.setPos(450, 300)
-        svg_item.setToolTip('MPRUN Course Element')
-
-        self.create_image_attributes(svg_item)
-
-    def import_course_element(self):
-        # Create Options
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-
-        # File Dialog, file path
-        file_dialog = QFileDialog()
-        file_dialog.setNameFilter("SVG files (*.svg)")
-
-        file_path, _ = file_dialog.getOpenFileName(self, "Import Course Element", "", "SVG Files (*.svg)",
-                                                   options=options)
-
-        if file_path:
-            # Get name for button
-            entry, ok = QInputDialog.getText(self, 'Import Course Element', 'Enter a name for the imported element:')
-            if ok:
-                # Add element
-                btn = QPushButton()
-                btn.setText(entry)
-                btn.clicked.connect(lambda: self.create_item(CustomSvgItem(file_path)))
-                self.layout.addWidget(btn)
-
-    def create_item(self, item):
-        self.canvas.addItem(item)
-        item.setToolTip('Imported SVG Item (Not an MPRUN Element)')
-        self.create_image_attributes(item)
-
-    def create_image_attributes(self, item):
-        item.setFlag(QGraphicsItem.ItemIsMovable)
-        item.setFlag(QGraphicsItem.ItemIsSelectable)
-        item.setZValue(0)
-
-    def closeEvent(self, event):
-        # Display a confirmation dialog
-        confirmation_dialog = QMessageBox()
-        confirmation_dialog.setIcon(QMessageBox.Warning)
-        confirmation_dialog.setText("Are you sure you want to close the Course Elements Picker? (This will delete imported Course Element spawn buttons)")
-        confirmation_dialog.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
-        confirmation_dialog.setDefaultButton(QMessageBox.No)
-
-        # Get the result of the confirmation dialog
-        result = confirmation_dialog.exec_()
-
-        # If the user clicked Yes, close the window
-        if result == QMessageBox.Yes:
-            event.accept()
-        else:
-            event.ignore()
 
 
 if __name__ == '__main__':
