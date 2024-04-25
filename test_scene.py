392a5b9a357915754117bd891109f880fe6d21d5
K-TECH Industries (Keller Hydle) <151480646+ktechhydle@users.noreply.github.com>
2024-04-25 07:57:49 -0600

    Update test_scene.py

--- test_scene.py
+++ test_scene.py
@@ -1,1 +1,15 @@
-print('hello!')
+from graphics_framework import *
+
+class MainWin(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle('MPRUN Beta Test Scene')
+        
+        
+        
+if __name__ == '__main__':
+    win = QApplication([])
+    app = MainWin()
+    app.show()
+    sys.exit()
