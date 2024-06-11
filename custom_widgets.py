9882b3b206a6998dbbda53225061a7593a596e54
kelle <kellerhydle@gmail.com>
2024-06-10 22:41:41 -0600

    #315 styled checkbox
    -added new icon
    -checkstates work
    -also changed horizontal seperator colors

--- custom_widgets.py
+++ custom_widgets.py
@@ -18,7 +18,8 @@ class HorizontalSeparator(QFrame):
         self.color = '#4b4b4b'
         self.setFrameShape(QFrame.HLine)
         self.setFrameShadow(QFrame.Plain)
-        self.setStyleSheet(f'background-color: {self.color}; ')
+        self.setStyleSheet(f'background-color: {self.color}; '
+                           f'color: {self.color}')
 
 class QColorButton(QPushButton):
     def __init__(self, parent=None):
