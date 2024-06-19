d8cd0ce519d153fde1b185c1fe2d025031731886
kelle <kellerhydle@gmail.com>
2024-06-18 22:55:05 -0600

    #375 new files
    -added more course elements
    -customized libraries.py to include a search bar

--- src/gui/libraries.py
+++ src/gui/libraries.py
@@ -8,6 +8,7 @@ class DragDropListWidget(QListWidget):
         super().__init__(parent)
         self.setDragEnabled(True)
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.all_items = []
 
     def startDrag(self, supportedActions):
         item = self.currentItem()
@@ -23,6 +24,13 @@ class DragDropListWidget(QListWidget):
             drag.setPixmap(pixmap)
 
             drag.exec_(Qt.CopyAction | Qt.MoveAction)
+
+    def filter_items(self, text):
+        for item in self.all_items:
+            item.setHidden(True)
+
+            if text.lower() in item.text().lower():
+               item.setHidden(False)
 
 class LibraryWidget(QWidget):
     def __init__(self, canvas, parent=None):
@@ -46,18 +54,25 @@ class LibraryWidget(QWidget):
         self.reload_library_button = QPushButton("")
         self.reload_library_button.setFixedWidth(28)
         self.reload_library_button.setStyleSheet('border: none')
-        self.reload_library_button.setIcon(QIcon('../../ui/UI Icons/refresh_icon.svg'))
+        self.reload_library_button.setIcon(QIcon('ui/UI Icons/refresh_icon.svg'))
         self.reload_library_button.setToolTip('Reload the current library')
-        library_btn_hlayout = ToolbarHorizontalLayout()
-        library_btn_hlayout.layout.addWidget(self.open_library_button)
-        library_btn_hlayout.layout.addWidget(self.reload_library_button)
+
+        # Search bar
+        self.search_bar = QLineEdit()
+        self.search_bar.setPlaceholderText("Search...")
+        self.search_bar.textChanged.connect(self.filter_library)
+
+        library_btn_hlayout = QHBoxLayout()
+        library_btn_hlayout.addWidget(self.open_library_button)
+        library_btn_hlayout.addWidget(self.reload_library_button)
+
+        self.layout.addLayout(library_btn_hlayout)
+        self.layout.addWidget(self.search_bar)
+        self.layout.addWidget(self.library_list_widget)
 
         # Connect button to the method
         self.open_library_button.clicked.connect(self.open_library)
         self.reload_library_button.clicked.connect(self.reload_library)
-
-        self.layout.addWidget(library_btn_hlayout)
-        self.layout.addWidget(self.library_list_widget)
 
     def open_library(self):
         # Open file dialog to select a folder
@@ -74,24 +89,33 @@ class LibraryWidget(QWidget):
     def load_svg_library(self, folder_path):
         # Clear existing items in the list widget
         self.library_list_widget.clear()
+        self.library_list_widget.all_items = []
 
-        # List all Combobox Images files in the selected folder
+        # List all SVG files in the selected folder
         svg_files = [f for f in os.listdir(folder_path) if f.endswith('.svg')]
 
-        # Check if no Combobox Images files are found
+        # Check if no SVG files are found
         if not svg_files:
             self.library_list_widget.setDragEnabled(False)
             list_item = QListWidgetItem('No files found')
-            list_item.setIcon(QIcon('../../ui/UI Icons/folder_failed_icon.svg'))
+            list_item.setIcon(QIcon('ui/UI Icons/folder_failed_icon.svg'))
             self.library_list_widget.setIconSize(QSize(40, 40))
             self.library_list_widget.addItem(list_item)
 
         else:
             self.library_list_widget.setDragEnabled(True)
-            # Add each Combobox Images file to the list widget
+            # Add each SVG file to the list widget
             for svg_file in svg_files:
                 list_item = QListWidgetItem(svg_file)
                 list_item.setData(Qt.UserRole, os.path.join(folder_path, svg_file))
                 list_item.setIcon(QIcon(os.path.join(folder_path, svg_file)))
                 self.library_list_widget.setIconSize(QSize(80, 80))
                 self.library_list_widget.addItem(list_item)
+                self.library_list_widget.all_items.append(list_item)
+
+    def filter_library(self, text):
+        try:
+            self.library_list_widget.filter_items(text)
+
+        except Exception as e:
+            print(e)
