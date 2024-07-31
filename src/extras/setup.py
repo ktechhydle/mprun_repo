990fa9053289497fb7660a6293077de528758bc9
kelle <kellerhydle@gmail.com>
2024-07-31 12:09:58 -0600

    #509 setup updates
    -modified setup.py

--- src/extras/setup.py
+++ src/extras/setup.py
@@ -1,6 +1,15 @@
 from setuptools import setup
 
+APP = ['app.py']
+DATA_FILES = [('internal data', ['internal data/_recent_files.json', 'internal data/_settings.json'])]
+OPTIONS = {
+    'argv_emulation': True,
+    'packages': ['PyQt5', 'vtracer', 'numpy', 'Markdown', 'scipy', 'scikit-image', 'shapely', 'pickle', 'json'],
+}
+
 setup(
-    app=['main.py'],
-    setup_requires=['py2app']
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
 )
