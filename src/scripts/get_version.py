c4510226fab221538ca4c491967c08aba4decf56
kelle <kellerhydle@gmail.com>
2024-10-03 16:33:11 -0600

    #767 ui updates
    -fixed slow requests for get_version.py

--- src/scripts/get_version.py
+++ src/scripts/get_version.py
@@ -6,12 +6,16 @@ from PyQt5.QtWidgets import QMessageBox
 def get_latest_version(parent):
     url = 'https://raw.githubusercontent.com/ktechhydle/mprun_repo/main/internal data/_version.txt'
     try:
-        response = requests.get(url)
+        # Set a timeout of 5 seconds
+        response = requests.get(url, timeout=2)
         if response.status_code == 200:
             return response.text.strip()  # Get the latest version from version.txt
         else:
             QMessageBox.warning(parent, 'Error', 'Unable to fetch the latest version.')
+    except requests.exceptions.Timeout:
+        QMessageBox.warning(parent, 'Error', 'The request timed out. Please check your internet connection.')
     except Exception as e:
         QMessageBox.warning(parent, 'Error', f'Failed to check for updates: {str(e)}')
 
+    # Return the current version if fetching the latest one fails
     return parent.canvas.mpversion
