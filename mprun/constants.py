29a98963a8f3eee56370e9ae12d05cf4b264656e
kelle <kellerhydle@gmail.com>
2024-11-13 10:02:29 -0700

    #948 mprun module updates
    -added new constants

--- mprun/constants.py
+++ mprun/constants.py
@@ -1,9 +1,12 @@
 from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QGraphicsItem
 
 TIP_WINDOW_Y_PADDING = -5
 TIP_WINDOW_X_PADDING = 10
 DEFAULT_PANEL_WIDTH = 280
 DEFAULT_LINE_EDIT_WIDTH = 200
+ITEM_SELECTABLE, ITEM_MOVABLE = (QGraphicsItem.GraphicsItemFlag.ItemIsSelectable,
+                                 QGraphicsItem.GraphicsItemFlag.ItemIsMovable)
 WINDOW_TYPE_TOOL = Qt.WindowType.Tool
 WINDOW_TYPE_POPUP = Qt.WindowType.Popup
 WINDOW_TYPE_TOOLTIP = Qt.WindowType.ToolTip
